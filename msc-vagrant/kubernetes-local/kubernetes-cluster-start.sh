847e6253d11961be0e58484090aee8b7ad73d59c
Daniel Louchansky <bekasik@gmail.com>
2016-05-20 12:39:36 +0300

    manual to setup kubernetes on local ubuntu vm

--- msc-vagrant/kubernetes-local/kubernetes-cluster-start.sh
+++ msc-vagrant/kubernetes-local/kubernetes-cluster-start.sh
@@ -1,23 +1,33 @@
-sudo apt-get install npm
-sudo apt-get install node
-sudo apt-get install nodejs-legacy
-export K8S_VERSION=$(curl -sS https://storage.googleapis.com/kubernetes-release/release/stable.txt)
-export ARCH=amd64
-docker run -d \
-    --volume=/:/rootfs:ro \
-    --volume=/sys:/sys:ro \
-    --volume=/var/lib/docker/:/var/lib/docker:rw \
-    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
-    --volume=/var/run:/var/run:rw \
-    --net=host \
-    --pid=host \
-    --privileged \
-    gcr.io/google_containers/hyperkube-${ARCH}:${K8S_VERSION} \
-    /hyperkube kubelet \
-        --containerized \
-        --hostname-override=127.0.0.1 \
-        --api-servers=http://localhost:8080 \
-        --config=/etc/kubernetes/manifests \
-        --cluster-dns=10.0.0.10 \
-        --cluster-domain=cluster.local \
-        --allow-privileged --v=2
+# should be executed on Vagrant ubuntu machine
+# http://kubernetes.io/docs/getting-started-guides/ubuntu/
+
+sudo mkdir /kube-sources
+cd /kube-sources
+sudo git clone https://github.com/kubernetes/kubernetes.git
+cd kubernetes/
+
+# set up component versions
+export KUBE_VERSION=1.1.8
+export FLANNEL_VERSION=0.5.0
+export ETCD_VERSION=2.2.0
+
+# edit cluster/ubuntu/config-default.sh
+
+export nodes="root@192.168.50.4" # set up one node
+export role="ai" # node is master and node
+export NUM_NODES=${NUM_NODES:-1}
+
+cd cluster/
+
+
+# set up some auth things if you're logged in as root user
+sudo -s
+# http://containertutorials.com/get_started_kubernetes/index.html
+ssh-keygen -t rsa
+# Your public key has been saved in /root/.ssh/id_rsa.pub.
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
+
+# run
+KUBERNETES_PROVIDER=ubuntu ./kube-up.sh
+cd cluster/ubuntu
+KUBERNETES_PROVIDER=ubuntu ./deployAddons.sh
